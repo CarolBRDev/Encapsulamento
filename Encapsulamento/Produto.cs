9a5f7ad0d619f1b84e59fa2a2fac78fc1f335a6f
Caroline Ramos <carolbeatrice@gmail.com>
2024-03-28 15:24:07 -0300

    criados get e set para classe Produto

--- Encapsulamento/Produto.cs
+++ Encapsulamento/Produto.cs
@@ -6,8 +6,10 @@ namespace Encapsulamento
 {
     class Produto
     {
+        const string NOME_EMPRESA = "Teste SA"; //nome de constantes sao sempre com todas as letras maiusculas
+        
         private string _nome; //atributo privado se escreve com _ e primeira minuscula: _name
-        private double _preco;
+        private double _preco; //deixar privado protege o atributo, nao permitindo manipulação pelo usuario
         private int _quantidade;
 
         public Produto (string nome, double preco)
@@ -16,8 +18,57 @@ namespace Encapsulamento
             _preco = preco;
         }
 
-        public Produto(string nome, double preco, int quantidade) : this(nome,preco) 
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
         {
             _quantidade = quantidade;
+        }
+
+        public string GetNome() //pega o nome, retorna uma string (string)
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome) //define novo valor, nao retornando nada ao usuário (void)
+        {
+            if(string.IsNullOrWhiteSpace(nome) || nome.Length < 3)
+            {
+                Console.WriteLine("Nome nulo ou com menos de 3 caracteres.");
+                return;
+            }
+
+            _nome = nome;
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public void SetPreco(double preco)
+        {
+            if (preco < 0)
+            {
+                Console.WriteLine("Preco nao pode ser negativo");
+                return;
+            }
+
+            _preco = preco;
+        }
+
+        public int GetQuantidade ()
+        {
+            return _quantidade;
+        }
+
+        public void SetQuantidade(int quantidade)
+        {
+            if (quantidade < 0)
+            {
+                Console.WriteLine("Quantidade nao pode ser negatica.");
+                return;
+            }
+
+            _quantidade = quantidade;
+        }
     }
 }
--- Encapsulamento/Program.cs
+++ Encapsulamento/Program.cs
@@ -9,6 +9,13 @@ namespace Encapsulamento
             Produto p1 = new Produto("Tijela", 1.99);
             Produto p2 = new Produto("Mc Book", 999.99, 10);
 
+            Console.WriteLine(p1.GetNome()); //usa um metodo que permite pegar o atributo privado
+
+
+
         }
+
+       
+
     }
 }
